c11efd51e8fd46c570be4011879bce286ee01f7c
Bibash Japrel <bibashjaprel111@gmail.com>
2025-05-26 20:53:28 +0545

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Link from 'next/link'
-import NewTransactionForm from './components/NewTransactionForm'
+import React from 'react'
+
 const page = () => {
-
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-50">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Your Expense Tracker</h1>
-
-    </main>
+    <div>Hello World - Home page</div>
   )
-
 }
 
 export default page
